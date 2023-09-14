52db8bc270cf03138c6b1e8e5de74ad3d86157a2
PedroCod3r <pedrohenriquelimalopes75@gmail.com>
2023-09-14 14:52:41 -0300

    Iniciando Projeto API

---
+++ API/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+from API.views import home
+
+# Create your views here.
+# def home (request):
+#     return HttpResponse("Hello Word, My name is Pedro!")
+
+# def home (request):
+    
