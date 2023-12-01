f0eb509b32f5d0b371bdb7711fa94973dd3674f8
PedroCod3r <pedrohenriquelimalopes75@gmail.com>
2023-12-01 17:03:57 -0300

    Refactor

--- web/static/script.js
+++ web/static/script.js
@@ -1,26 +1,52 @@
-function render_element (plataforma, ouvidoria, mensagem) {
-    return `
-    <div class="plataform-feeds">
-    
-        <div class= "container-plataforma"> 
-            <h2>Plataforma: ${plataforma}</h2>
-        </div>
-        <div class = "container-ouvidoria">
-            <h2>Ouvidoria: ${ouvidoria}</h2>
-        </div>
+// window.addEventListener('load', function () {
+//     const feedlist = document.getElementById('feed-list')
+//     // alert('hello word!')
+//     fetch('http://127.0.0.1:8000/api/feeds/')
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//         // console.log(resp)
+//         const feeds = data.map(feed => {
+//             const listitem = document.createElement('div')
+//             listitem.className = 'feed-list'
+//             listitem.textContent = `${feed.mensagem_digitada_na_ouvidoria} - ${feed.plataforma.nome}, ${feed.ouvidoria.nome}`
+//             return listitem
+//         })
+        
+//         feeds.forEach(list => {
+//             feedlist.appendChild(list)
+//         })
 
-        <div class = "container-mensagem">
-            <p>Mensagem Digitada: ${mensagem}</p>
-        </div>
+//     })
+// })
 
-    </div>`
-}
-
-function render_feedback() {
+window.addEventListener('load', function () {
+    const feedlist = document.getElementById('feed-list')
+    // alert('hello word!')
     fetch('http://127.0.0.1:8000/api/feeds/')
     .then((resp) => resp.json())
-    .then((resp) => {
-        const feeds = resp.map(feed => render_element(feed.plataforma, feed.ouvidoria, feed.mensagem))
-        document.getElementById('feed-list').innerHTML = feeds.join()
+    .then((data) => {
+        console.log(data)
+        // console.log(resp)
+       const feeds = data.map(feed => {
+        const listitem = document.createElement('div')
+        listitem.className = 'feed-list'
+        listitem.innerHTML = `
+            <h2>${feed.plataforma.nome}</h2>
+            <h3>${feed.ouvidoria.nome}</h3>
+            <p>${feed.mensagem_digitada_na_ouvidoria}</p>`
+        return listitem
+        })
+        feeds.forEach(list => {
+            feedlist.appendChild(list)
+        })
     })
-}
+})
+
+// function render_feedback () {
+//     fetch('http://127.0.0.1:8000/api/feeds/')
+//     .then((resp) => resp.json())
+//     .then((resp) => {
+//         const feeds = resp.map(feed => render_element(feed.plataforma, feed.ouvidoria, feed.mensagem))
+//         document.getElementById('feed-list').innerHTML = feeds.join()
+//     })
+// }
